632797e7cbb646a71a8ce05d3b3abc1fb83e8033
Dante Annetta <danteannetta03@gmail.com>
2024-03-27 18:57:41 -0300

    Create setup-zsh.sh

---
+++ setup-zsh.sh
@@ -0,0 +1,12 @@
+# WARNING: OH MY ZSH MUST BE INSTALLED BEFORE
+
+# path to the .zshrc file
+
+#file="$HOME/.zshrc"
+
+# set default theme for zsh
+
+#sed -i 's/^ZSH_THEME=.*/ZSH_THEME="dieter"/' "$file"
+
+# add cls alias to the file
+#echo 'alias cls="clear"' >> "$file"
